637a8c5978034806482fc2cc065a023a1a1f83f1
Linh Vu <prodige112002@gmail.com>
2022-01-25 20:08:42 +0700

    finish all

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
+    // sticky-nav 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $("header").addClass("sticky-scroll");
+        } else {
+            $("header").removeClass("sticky-scroll");
+        }
+    })
 
+    // sign in button
+    $(".btn-sign-in").click(function() {
+        $(".sign-in").addClass("is-active");
+    })
+    $(".exit-sign-in").click(function() {
+        $(".sign-in").removeClass("is-active");
+    })
+   
     // movie-slider
     var sliderMain = $(".trending-list");
     $(".go-right").click(function() {
         sliderMain.append($(".trending-item:first"));
-    })
-    $(".go-left").click(function() {
-        var temp = sliderMain.pop();
-        console.log(temp);
     })
 
     // responsive hamburger menu
@@ -22,14 +34,15 @@ $(document).ready(function() {
     })
     
     // scroll-bar
+    $(".progress .progress-value p").text("0%");
     $(document).scroll(function() {
         var scrollProgress = $(".progress");
-        var progressValue = $(".progress-value");
         var pos = $(document).scrollTop();
         var calcHeight = $(document).height() - $(window).height();
         var scrollValue = Math.round(pos * 100 / calcHeight);
         
-        scrollProgress.css("background", "conic-gradient(#e70634 " + scrollValue + "%, #2b2f38 " + scrollValue + "%)");
+        $(".progress .progress-value p").text(scrollValue + "%");
+        scrollProgress.css("background", "conic-gradient(#00ccff " + scrollValue + "%, #2b2f38 " + scrollValue + "%)");
     })
 })
 
